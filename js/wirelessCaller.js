65624b336bff588f0efb731d587c21994e536db4
josedlca <jose.dlcruz.ayala@gmail.com>
2019-03-05 18:59:07 -0500

    start of second face

--- js/wirelessCaller.js
+++ js/wirelessCaller.js
@@ -27,3 +27,15 @@ window.addEventListener('scroll',function(){
         arrowUp.classList.add('hide-arrow');
     }
 });
+
+
+// SendForm tester
+
+let sendForm = document.querySelector('#sendForm');
+let contactsend = document.querySelector('.contact__send');
+let contactForm = document.querySelector('.contact__form');
+
+sendForm.addEventListener('click',function(){
+    contactForm.classList.add('quiteform');
+    contactsend.classList.add('addmessage');
+});
