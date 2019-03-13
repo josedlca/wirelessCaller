64db06fd4d466ba890cd114ad63662a21c7eab0d
josedlca <jose.dlcruz.ayala@gmail.com>
2019-03-13 00:22:46 -0500

    again

--- js/sliderCustom.js
+++ js/sliderCustom.js
@@ -11,8 +11,7 @@ slidr.create('carousel', {
     pause: true,
     transition: 'linear'
   }).add('h', ['one', 'two', 'three', 'one'])
-  .start();
-  // .auto(4000)
+  .start().auto(4000);
 function number(picid){
   let divone = document.querySelector(".insideImg__text");
   let divoneimg = document.querySelector(".insideImg__img")
